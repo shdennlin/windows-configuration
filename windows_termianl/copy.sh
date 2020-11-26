5af839d5ab7e42260e22970e66fcac4a33d7d84e
Shawn Dennis Lin <shawndennislin@gmail.com>
2020-11-27 01:53:06 +0800

    modify copy.sh and theme(settings.json)

--- windows_termianl/copy.sh
+++ windows_termianl/copy.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-path='/mnt/d/windows-configuration/windows_termianl/'
-cp ./settings.json $path
-cp ./copy.sh $path
-echo "copy to $path"
+# path='/mnt/d/windows-configuration/windows_termianl/'
+SettingFileLocation='/mnt/c/Users/Shawn/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json'
+cp $SettingFileLocation .
+echo "copy settings.json to ."
